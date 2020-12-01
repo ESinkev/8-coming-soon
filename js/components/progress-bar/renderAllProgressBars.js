77edad312961953b39acd04396af9c5a0dd9964c
ESinkev <73878808+ESinkev@users.noreply.github.com>
2020-12-01 15:16:09 +0200

    Scrolling and loading progress bars

--- js/components/progress-bar/renderAllProgressBars.js
+++ js/components/progress-bar/renderAllProgressBars.js
@@ -3,13 +3,13 @@ import { isValidSingleProgressBar } from './isValidSingleProgressBar.js';
 import { renderProgressBar } from './renderProgressBar.js';
 
 /**
+ * 
  * @param {Object[]} data
  * @param {string} data[]. selector CSS like selectorius, kaip rasti vieta, kur suregeruoti turini
  * @param {string} data[]. title Progress bar pavadinimas
  * @param {number} data[]. value Progress bar reiksme procentais
  * @returns {boolean} Funkcijai tinkamai suveikus grazinas `true`, priesingu atveju - `false`
  */
-
 function renderAllProgressBars(data) {
     if (!isValidProgressBarsList(data)) {
         return false;
@@ -24,6 +24,20 @@ for (let i = 0; i < data.length; i++) {
 
     renderProgressBar(bar.selector, bar.title, bar.value);
 }
+
+const allProgressBars = document.querySelectorAll('.progress-bar');
+console.log(allProgressBars);
+
+addEventListener('sroll', () => {
+    const screenBottom = innerHeight + scrollY;
+
+    for (let bar of allProgressBars) {
+        const barBottom = bar.offsetHeight + bar.offsetTop;
+        if (screenBottom >= barBottom) {
+            bar.classList.add('animate');
+        }
+    }
+})
 
 return true;
 }
